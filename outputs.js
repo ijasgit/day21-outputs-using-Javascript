6a4113a49d74082a8ec7d1566fdcd262e1035c76
Ijasgit <mohammedijasn3899@gmail.com>
2022-06-27 22:07:24 +0530

    .

---
+++ outputs.js
@@ -0,0 +1,22 @@
+var num=10+20+"30";
+console.log(num)
+
+var num="10"+20+30;
+console.log(num)
+
+if(10=="10")
+{
+    console.log("true");
+}
+else(
+    console.log("false")
+)
+
+
+if(20==="20")
+{
+    console.log("true");
+}
+else(
+    console.log("false")
+)
